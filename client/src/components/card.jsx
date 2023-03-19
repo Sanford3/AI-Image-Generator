2dd66e1aea4ddd04a38a88880fe271edd07ed8e0
Viveksharma121 <114184360+Viveksharma121@users.noreply.github.com>
2023-03-19 22:16:11 +0530

    Update card.jsx

--- client/src/components/card.jsx
+++ client/src/components/card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { download } from "../assets";
 import { downloadImage } from "../utils";
+import {useState} from 'react';
 const Card = ({ _id, name, prompt, photo }) => {
   return (
     <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card">
